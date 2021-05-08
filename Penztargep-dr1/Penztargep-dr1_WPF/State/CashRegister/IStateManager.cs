85e994075eab83f500159ee69a2792b5d91a757f
chon <balint.ducsai@gmail.com>
2021-05-09 00:43:06 +0200

    Sync sprint 06/2

---
+++ Penztargep-dr1/Penztargep-dr1_WPF/State/CashRegister/IStateManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Penztargep_dr1_WPF.State.CashRegister {
+    public interface IStateManager {
+        public string ControlButtonText { get; set; }
+        public bool CurrentState { get; set; }
+        public ICommand ToggleState { get; }
+        
+    }
+}
---
+++ Penztargep-dr1/Penztargep-dr1_WPF/State/CashRegister/StateManager.cs
@@ -0,0 +1,51 @@
+﻿using Penztargep_dr1_WPF.Commands;
+using Penztargep_dr1_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Penztargep_dr1_WPF.State.CashRegister {
+    public class StateManager : ObservableObject, IStateManager {
+
+        private string _controlButtonText;
+        public string ControlButtonText {
+            get {
+                if (CurrentState) {
+                    _controlButtonText = "Pénztár zárása";
+                    return _controlButtonText;
+                }
+                _controlButtonText = "Pénztár nyitása";
+                return _controlButtonText;
+            } set {
+                _controlButtonText = value;
+                OnPropertyChanged(nameof(ControlButtonText));
+            } 
+        }
+
+        private bool _currentState;
+        public bool CurrentState {
+            get {
+                return _currentState;
+            }
+            set {
+                _currentState = value;
+                OnPropertyChanged(nameof(CurrentState));
+                OnPropertyChanged(nameof(ControlButtonText));
+            }
+        }
+        private ICommand _toggleState;
+        public ICommand ToggleState {
+            get {
+                if (_toggleState == null) {
+                    _toggleState = new RelayCommand(
+                        parameter => {
+                            CurrentState = !CurrentState;
+                        },
+                        parameter => true);
+                }
+                return _toggleState;
+            }
+        }
+    }
+}
